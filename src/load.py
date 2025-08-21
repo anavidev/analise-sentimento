51e370d3f3da1999ef086f7d648485fb903d2b10
anavidev <anavi.oliveira56@gmail.com>
2025-08-21 20:13:17 -0300

    desenvolvimento pipeline etl com dados do algoritmo ml-based

--- src/load.py
+++ src/load.py
@@ -1,3 +1,4 @@
 from transform import *
 
-arquivo_csv.to_csv('data/processed/store_product_reviews_transformed.csv', index=False, sep=',')
+arquivo_csv_rb.to_csv('data/processed/store_product_reviews_transformed.csv', index=False, sep=',')
+arquivo_csv_mlb.to_csv('data/processed/amazon_reviews_transformed.csv', index=False, sep=';')
