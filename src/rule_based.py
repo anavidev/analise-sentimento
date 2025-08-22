7ad36503fc4c9dbfcb120743e588c12182eb41cf
anavidev <anavi.oliveira56@gmail.com>
2025-08-22 02:14:56 -0300

    desenvolvimento do algoritmo ml-based e correcao de erros

--- src/rule_based.py
+++ src/rule_based.py
@@ -6,8 +6,8 @@ from sklearn.metrics import confusion_matrix, f1_score
 
 analisador = SentimentIntensityAnalyzer()
 
-dt = pd.read_csv("data/processed/store_product_reviews_transformed.csv", sep = ',')
-verificar_dataset("Dataset importado com sucesso.",dt,"Teste")
+dt = pd.read_csv("data/processed/store_product_reviews_transformed.csv", sep = ';')
+verificar_dataset("Dataset importado com sucesso.",dt,"Store Product")
 
 print("Análise em andamento.")
 
@@ -16,7 +16,7 @@ dt['pontuacao_vader'] = dt['comentario'].apply(lambda texto: analisador.polarity
 
 # extrair compound de cada comentario e classifica-lo
 dt['compound_vader'] = dt['pontuacao_vader'].apply(lambda x: x['compound'])
-dt['sentimento_novo'] = dt['compound_vader'].apply(
+dt['sentimento_pred'] = dt['compound_vader'].apply(
     lambda x: 1 if x >= 0.05
     else -1 if x <= -0.05
     else 0
@@ -25,15 +25,21 @@ dt['sentimento_novo'] = dt['compound_vader'].apply(
 # resultados
 print("\nEstatísticas do Algoritmo Rule-Based\n")
 
-print("Acuracia:")
-acuracia = np.mean(dt['sentimento_novo'] == dt['sentimento'])
+print("Quantidade de comentarios por cada sentimento antes da classificacao - DATASET DE TESTE:")
+print(dt.groupby('sentimento')['compound_vader'].count())
+
+print("\nQuantidade de comentarios por cada sentimento depois da classificacao - DATASET DE TESTE:")
+print(dt.groupby('sentimento_pred')['compound_vader'].count())
+
+print("\nAcuracia:")
+acuracia = np.mean(dt['sentimento_pred'] == dt['sentimento'])
 print(f'{acuracia:.4f} = {acuracia * 100:.2f}%')
 
 print("\nF1-Score:")
-print(f"{f1_score(dt['sentimento'], dt['sentimento_novo'], average='weighted'):.3f}")
+print(f"{f1_score(dt['sentimento'], dt['sentimento_pred'], average='weighted'):.3f}")
 
 print("\nMatriz de Confusão:")
-matriz = confusion_matrix(dt['sentimento'], dt['sentimento_novo'])
+matriz = confusion_matrix(dt['sentimento'], dt['sentimento_pred'])
 print(f"""Valores Negativos Corretos = {matriz[0][0]}
 Valores Neutros Corretos = {matriz[1][1]} 
 Valores Positivos Corretos = {matriz[2][2]}\n
@@ -44,13 +50,3 @@ Valores Neutros classificados como Positivos = {matriz[1][2]}
 Valores Positivos classificados como Negativos = {matriz[2][0]}
 Valores Positivos classificados como Neutros = {matriz[2][1]}
 """)
-
-print("\nQuantidade de comentarios por cada sentimento antes da classificacao:")
-print(dt.groupby('sentimento')['compound_vader'].count())
-
-print("\nQuantidade de comentarios por cada sentimento depois da classificacao:")
-print(dt.groupby('sentimento_novo')['compound_vader'].count())
-
-# carregar resultado
-dt = excluir_coluna(dt,'pontuacao_vader',"Teste")
-dt.to_csv('data/processed/store_product_reviews_rb.csv', index=False, sep=',')
--- src/transform.py
+++ src/transform.py
@@ -3,7 +3,7 @@ from utils import *
 
 ## limpeza, validacao e transformacao dos dados
 
-# dataframe de teste
+# dataframe do rule-based
 
 # traducao de colunas
 traducao_colunas = {
@@ -14,86 +14,27 @@ traducao_colunas = {
     'Summary': 'comentario',
     'Sentiment': 'sentimento'
 }
-dt_rb = dt_rb.rename(traducao_colunas, axis = 1)
-
-# retirar linhas e colunas desnecessarias do dataset
-dt_rb = excluir_coluna(dt_rb,'nome_produto',"Rule-Based")
-dt_rb = excluir_coluna(dt_rb,'preco_produto',"Rule-Based")
-dt_rb = excluir_coluna(dt_rb,'pontuacao',"Rule-Based")
-dt_rb = excluir_coluna(dt_rb,'avaliacao',"Rule-Based")
-
-colunas = dt_rb.columns
+dt = dt.rename(traducao_colunas, axis = 1)
+colunas = dt.columns
 
 # validacao e limpeza dos dados
-dt_rb = valores_ausentes(dt_rb,"Rule-Based")
-dt_rb = duplicatas(dt_rb,colunas,"Rule-Based")
+dt = valores_ausentes(dt,"Store Product")
+dt = duplicatas(dt,colunas,"Store Product")
 
-traducao_valores_booleanos(dt_rb,'sentimento')
+# retirar linhas e colunas desnecessarias do dataset
+dt = excluir_coluna(dt,'nome_produto',"Store Product")
+dt = excluir_coluna(dt,'preco_produto',"Store Product")
+dt = excluir_coluna(dt,'pontuacao',"Store Product")
+dt = excluir_coluna(dt,'avaliacao',"Store Product")
+colunas = dt.columns
 
-dt_rb.to_csv('data/processed/store_product_reviews_transformed.csv', index=False, sep=',')
+traducao_valores_booleanos(dt,'sentimento')
+dt = balancear_classes(dt,'sentimento',"Store Product")
+
+dt.to_csv('data/processed/store_product_reviews_transformed.csv', index=False, sep=',')
 
 # verificacao final de tratamento de dados
-arquivo_csv_rb = pd.read_csv('data/processed/store_product_reviews_transformed.csv', sep=',')
-arquivo_csv_rb = valores_ausentes(arquivo_csv_rb,"Rule-Based")
-arquivo_csv_rb = duplicatas(arquivo_csv_rb,colunas,"Rule-Based")
-verificar_dataset("Verificacao final.",arquivo_csv_rb,"Rule-Based")
+arquivo_csv = pd.read_csv('data/processed/store_product_reviews_transformed.csv', sep=',')
+verificar_dataset("Verificacao final.",arquivo_csv,"Store Product")
 
-
-# dataframe de treinamento
-
-traducao_colunas = {
-    'Text': 'comentario',
-    'cleaned_review': 'comentario_limpo',
-    'Score': 'avaliacao',
-    'sentiment_score': 'pontuacao',
-    'sentiment_category': 'sentimento',
-    'fear': 'medo',
-    'anger': 'raiva',
-    'anticip': 'antecipacao',
-    'trust': 'confianca',
-    'surprise': 'surpresa',
-    'positive': 'positividade',
-    'negative': 'negatividade',
-    'sadness': 'tristeza',
-    'disgust': 'nojo',
-    'joy': 'alegria',
-    'topic': 'topico'
-}
-dt_mlb = dt_mlb.rename(traducao_colunas, axis = 1)
-
-
-dt_mlb = excluir_coluna(dt_mlb,'comentario_limpo',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'avaliacao',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'pontuacao',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'medo',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'raiva',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'antecipacao',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'confianca',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'surpresa',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'positividade',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'negatividade',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'tristeza',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'nojo',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'alegria',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'topico',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'cluster',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'pca_1',"ML-Based")
-dt_mlb = excluir_coluna(dt_mlb,'pca_2',"ML-Based")
-
-colunas = dt_mlb.columns
-
-
-dt_mlb = valores_ausentes(dt_mlb,"ML-Based")
-dt_mlb = duplicatas(dt_mlb,colunas,"ML-Based")
-
-traducao_valores_booleanos(dt_mlb,'sentimento')
-
-dt_mlb.to_csv('data/processed/amazon_reviews_transformed.csv', index=False, sep=';')
-
-
-arquivo_csv_mlb = pd.read_csv('data/processed/amazon_reviews_transformed.csv', sep=';')
-arquivo_csv_mlb = valores_ausentes(arquivo_csv_mlb,"ML-Based")
-arquivo_csv_mlb = duplicatas(arquivo_csv_mlb,colunas,"ML-Based")
-verificar_dataset("Verificacao final.",arquivo_csv_mlb,"ML-Based")
-
-print("Transformação concluida.")
+print("Transformacao concluida.")
